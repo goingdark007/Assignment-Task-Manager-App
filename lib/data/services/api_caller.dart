bb0e9930388e3131dba4c32d51feb706f2c8f000
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-03 18:30:59 +0600

    Added safeDecode() so jsonDecode() does not crash

--- lib/data/services/api_caller.dart
+++ lib/data/services/api_caller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:logger/logger.dart';
 import 'dart:convert';
 import 'package:http/http.dart';
@@ -23,25 +24,27 @@ class ApiCaller {
       Uri uri = Uri.parse(url);
       _logRequest(url);
 
-      Response response = await get(uri, headers: {
+      final Response response = await get(uri, headers: {
+        'Content-Type': 'application/json',
         'token' : _tokenSource.accessToken ?? ''
-      });
+      }).timeout(const Duration(seconds: 15)); // Set a timeout for the request
+
       _logResponse(url, response);
 
       final int statusCode = response.statusCode;
-
+      final decodedData = _safeDecode(response.body);
       if (statusCode == 200) {
-        final decodedData = jsonDecode(response.body);
+
         return APIResponse(
             body: decodedData, responseCode: statusCode, isSuccess: true
         );
       } else if (response.statusCode == 401){
         await moveToLogin();
         return APIResponse(
-            body: jsonDecode(response.body), responseCode: statusCode, isSuccess: false, errorMessage: 'Unauthorized'
+            body: decodedData, responseCode: statusCode, isSuccess: false, errorMessage: 'Unauthorized'
         );
       } else {
-        final decodedData = jsonDecode(response.body);
+        debugPrint('Error response body: $statusCode\n body => ${response.body}'); // Log the error response body for debugging
         return APIResponse(
             body: decodedData, responseCode: statusCode, isSuccess: false
         );
@@ -53,7 +56,6 @@ class ApiCaller {
   }
 
   static Future<APIResponse> postRequest ({required String url, required Map<String, dynamic>? body}) async {
-
 
     try {
 
@@ -67,28 +69,27 @@ class ApiCaller {
         'token' : _tokenSource.accessToken ?? ''
       };
 
-      Response response = await post(
+      final Response response = await post(
           uri,
           headers: headers,
           body: body != null ? jsonEncode(body) : null
-      );
+      ).timeout(const Duration(seconds: 15)); // Set a timeout for the request
 
       _logResponse(url, response);
 
       final int statusCode = response.statusCode;
-
+      final decodedData = _safeDecode(response.body);
       if (statusCode == 200 || statusCode == 201) {
-        final decodedData = jsonDecode(response.body);
+
         return APIResponse(
             body: decodedData, responseCode: statusCode, isSuccess: true
         );
       } else if (response.statusCode == 401){
         await moveToLogin();
         return APIResponse(
-            body: jsonDecode(response.body), responseCode: statusCode, isSuccess: false, errorMessage: 'Unauthorized'
+            body: decodedData, responseCode: statusCode, isSuccess: false, errorMessage: 'Unauthorized'
         );
       } else {
-        final decodedData = jsonDecode(response.body);
         return APIResponse(
             body: decodedData, responseCode: statusCode, isSuccess: false
         );
@@ -108,9 +109,12 @@ class ApiCaller {
 
   static void _logResponse(String url, Response response) {
 
-    final decodedData = jsonDecode(response.body);
+    final decodedData = _safeDecode(response.body);
 
-    final prettyJson = const JsonEncoder.withIndent(' ').convert(decodedData);
+    final prettyJson = decodedData is String
+        ? decodedData
+        : const JsonEncoder.withIndent('  ').convert(decodedData);
+
 
     _logger.i(
       'URL1 => $url\n'
@@ -120,10 +124,20 @@ class ApiCaller {
     );
   }
 
+  /// Safely JSON decoding
+  static dynamic _safeDecode(String body) {
+    try {
+      return jsonDecode(body);
+    } catch (_) {
+      return body; // fallback (plain text / HTML)
+    }
+  }
+
   static Future<void> moveToLogin () async {
     await AuthController.clearUserData();
     TaskManagerApp.navigatorKey.currentState?.pushNamedAndRemoveUntil('/login', (route) => false);
   }
+
 
 }
 
