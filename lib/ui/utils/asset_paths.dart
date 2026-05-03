1e8cee1cd8ee0212fa42d45e191e0eb4648f4028
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-05-03 12:13:36 +0600

    Just updated the logo

--- lib/ui/utils/asset_paths.dart
+++ lib/ui/utils/asset_paths.dart
@@ -3,6 +3,6 @@ class AssetPaths {
   static final String _imagePath = 'assets/images';
 
   static String backgroundSVG = '$_imagePath/background.svg';
-  static String logoSVG = '$_imagePath/task.svg';
+  static String logoSVG = '$_imagePath/task_logo.svg';
 
 }
